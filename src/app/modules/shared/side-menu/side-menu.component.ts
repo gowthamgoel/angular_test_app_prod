e8997ab9c03d709acba8cdf0bb51a71bf9eddbc6
gowthamgoel <goelgowtham@gmail.com>
2024-08-09 23:44:23 +0530

    basic layout

---
+++ src/app/modules/shared/side-menu/side-menu.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.scss'
+})
+export class SideMenuComponent {
+  isMenuOpen = false;
+  pages = [
+    {
+      name: 'Home',
+      route: '/home'
+    },
+    {
+      name: 'Test',
+      route: '/cr'
+    }
+  ];
+
+  openMenu() {
+    this.isMenuOpen = true;
+    document.getElementById('menuOverlay')!.style.display = 'block';
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+    document.getElementById('menuOverlay')!.style.display = 'none';
+  }
+}
